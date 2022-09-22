ddc6219baf248ef9d9dba4ba488d505fd2341cff
MWIYIKINWA ISAAC <isaacmwiyikinwa08@gmail.com>
2022-09-22 10:41:00 +0300

    fifth commit

--- header.php
+++ header.php
@@ -10,19 +10,18 @@ $linksResults = $content->getLinks();
   <title>kbrc Wakiso</title>
 
      <link rel="shortcut icon" type="image/png" href="assets/favlogo.png">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css" >
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css?v=1" >
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" >
     <link rel="stylesheet" type="text/css" href="assets/css/swiper-bundle.min.css" >
     <link rel="stylesheet" type="text/css" href="assets/css/all.css">
     <script src="assets/js/all.js"></script>
-
 </head>
 <body>
 
   <style>
 
     /* navbar responsive */
-         @media screen and (max-width:900px){
+         @media screen and (max-width:780px){
             .menuItems a{
                display: block;
                float: right;
@@ -32,6 +31,11 @@ $linksResults = $content->getLinks();
               display:inline-block;
               margin-left:10px;
             }
+
+            .container{
+
+            }
+
               }
 
       
@@ -80,19 +84,24 @@ $linksResults = $content->getLinks();
 
    <!-- header  -->
   
-   <header>
-    <img src="assets/images/Logo.png" class="logo">
- <div class="menu">
-      <div class="menuItems">
-          <a href="index.php">Home</a>
-          <a href="contact.php">Contact</a>
-          <a href="contact.php#giving">Giving</a>
-          <a href="dowbi.php">DOWBI</a>
-         <a href="index.php#joinUS" class="fw-bold" style="color: #0dcaf0;">JOIN-US</a> 
-         </div>
-    </div>
-    
-</header>
+
+<div class="container">
+<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between  mb-2 ">
+      <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
+      </a>
+
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="index.php" class="nav-link px-2 text-dark active fw-bold ">Home</a></li>
+        <li><a href="dowbi.php" class="nav-link px-2 text-dark active fw-bold">Dowbi</a></li>
+        <li><a href="index.php#joinUS" class="nav-link px-2 text-info active fw-bold">JOIN US</a></li>
+      </ul>
+
+      <div class="col-md-3 text-end give">
+        <button type="button" class="btn btn-outline-info my-2">Give</button>
+      </div>
+    </header>
+</div>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-info">
     <div class="container">
--- tv.php
+++ tv.php
@@ -11,13 +11,7 @@ include('header.php');
 
 </style>
 
-<?php
-   $api_key = 'AIzaSyBl0g_u4abY6OG2p1BXjiU2iTkFOnSwpqg';
-   $channel_id = 'UC0cMoHoIth98CTusXupc5gw';
-   $url = 'https://www.googleapis.com/youtube/v3/search?key='.$api_key.'&channelId='.$channel_id.'&part=snippet,id&order=date';
 
-   $videos = file_get_contents($url);
-?>
 
 
 <div class="container">
@@ -28,17 +22,13 @@ include('header.php');
           </div>
           <hr>
         </div>
-        <?php // if(!empty($videos)){   $videos_arr = json_decode($videos, true); ?>
-          <?php // if(!empty($videos_arr['items'])){  ?>
-            <?php   //foreach($videos_arr['items'] as $vids){ ?>
               
         <div class="row my-4">
 
-        <?php  //if($vids['id']['kind'] == 'youtube#video'){  ?>
-           <!-- <div class="col-lg-4 col-md-6 col-sm-12 my-2 vid">
+            <div class="col-lg-4 col-md-6 col-sm-12 my-2 vid">
               
-           </div> -->
-           <iframe width="560" height="315" src="https://www.youtube.com/embed/yjhqiZ-cJ4A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+           </div>
+           <iframe width="560" height="800" src="https://www.youtube.com/embed/yjhqiZ-cJ4A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
        
         </div>
        
